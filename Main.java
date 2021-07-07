8bfb72f89482ecea1c008dcfa3eb079440ff1203
Ryan Klaproth <ryanklaproth@gmail.com>
2021-07-07 02:52:38 +0000

    Added game.java file

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+
+    //initialize any global variables needed for the game
+
+    //initialize players
+
+    //initialize game board
+
+    //play game
   }
 }
